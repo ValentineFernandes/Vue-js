444a4aae48880fe65a803bd8e08addf539cf8d74
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-11 22:25:27 -0800

    Create hello.vue

---
+++ hello.vue
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<body>
+
+<div id="app">
+  <h1>{{ message }}</h1>
+</div>
+
+<script>
+var myObject = new Vue({
+  el: '#app',
+  data: {message: 'Hello Vue!'}
+})
+</script>
+
+</body>
+</html>
