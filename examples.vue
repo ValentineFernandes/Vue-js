06debcb31e0b6f1b366a787403486ef7e70b6a78
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-11 22:28:28 -0800

    Create examples.vue

---
+++ examples.vue
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<body>
+
+<h2>Vue.js</h2>
+
+<div id="app">
+  <ul>
+    <li v-for="x in todos">
+      {{ x.text }}
+    </li>
+  </ul>
+</div>
+
+<script>
+myObject = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue.js' },
+      { text: 'Build Something Awesome' }
+    ]
+  }
+})
+</script>
+
+</body>
+</html>
