114d55b00ed95f0223894447ec7d238543f6238e
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-11 22:27:42 -0800

    Create object.vue

---
+++ object.vue
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<body>
+
+<h2>Vue.js</h2>
+
+<div id="app">
+  <p>{{ message }}</p>
+  <p><input v-model="message"></p>
+</div>
+
+<script>
+myObject = new Vue({
+  el: '#app',
+  data: {message: 'Hello Vue.js!'}
+})
+</script>
+
+</body>
+</html>
